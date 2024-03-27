229c8d7a53112076a1df10af632e8bea07746a6d
chirag <chiragdhiman00067@gmail.com>
2024-03-27 22:26:41 +0530

    create ui and functionality for sign in

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import { loadFontSync } from 'figlet';
+
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import {Link , useNavigate} from "react-router-dom"
