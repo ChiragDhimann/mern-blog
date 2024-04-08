f06bb14153c5cf7cd1ec39b774d32d1d8562ea5e
chirag <chiragdhiman00067@gmail.com>
2024-04-09 00:56:26 +0530

    complete create post page ui

--- client/src/components/Dashprofile.jsx
+++ client/src/components/Dashprofile.jsx
@@ -9,12 +9,13 @@ import { updateFailure,updateStart,updateSuccess,deleteUserError,deleteUserStart
 import {useDispatch} from 'react-redux'
 import { Modal } from 'flowbite-react';
 import {HiOutlineExclamationCircle} from 'react-icons/hi'
+import {Link} from  'react-router-dom'
 
 
 
 
 export default function Dashprofile() {
-  const {currentUser,error}= useSelector(state => state.user)
+  const {currentUser,error,loading}= useSelector(state => state.user)
   const [imageFile,setImageFile]=useState(null)
   const [imageFileUrl,setImageFileUrl]=useState(null)
   const [imageFileUploadProgress,setImageFileUploadProgress]=useState(null);
@@ -212,9 +213,18 @@ setImageFileUploadError(null);
       <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
       <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
       <TextInput type='password' id='password' placeholder='password'  onChange={handleChange}/>
-      <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-        Update
+      <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+        {loading ? '...loading': "update"}
       </Button>
+
+        { currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+          <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>
+            Create a post
+          </Button>
+          </Link>
+        )}
+
       </form>
       <div className='text-red-500 flex justify-between mt-3'>
         <span onClick={()=>{setShowModal(true)}} className='cursor-pointer'>Delete Account</span>
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {TextInput,Select, FileInput, Button} from 'flowbite-react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export default function CreatePost() {
+  return (
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+    
+    <h1 className='text-center text-3xl my-7 font-semibold'>Create a new post</h1>
+    <form className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+            <TextInput type='text' placeholder='title' required id='title' className='flex-1'/>
+            <Select>
+                <option value='uncategorized'>Select a category</option>
+                <option value='javascript'>Javascript</option>
+                <option value='reactjs'>React.js</option>   
+                <option value='nextjs'>Next.js</option>
+            </Select>
+            
+        </div>
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+                <FileInput accept="image/*" type='file' />
+                <Button gradientDuoTone='purpleToBlue' type='button' size='sm' outline>Upload File</Button>
+            </div>
+            <ReactQuill  theme="snow" className='h-72 mb-12' required/>
+            <Button type='submit' gradientDuoTone='purpleToPink' >Publish</Button>
+    </form>
+        </div>
+  )
+}
