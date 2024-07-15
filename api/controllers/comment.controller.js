85b98313d3c6d8dfc75ce72dd9c0803518b24152
chirag <chiragdhiman00067@gmail.com>
2024-07-15 23:26:30 +0530

    show the comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,22 +1,32 @@
 import { errorhandler } from "../utils/error.js";
 import Comment from "../models/comment.model.js";
 
-export const createComment=async (req,res,next)=>{
-    try{
-        const {userId,postId,content}=req.body;
-        if(userId!==req.user.id){
-            return next(errorhandler(403,"you are not allowed to create a comment"))
-        }
-
-        const newComment=new Comment({
-            content,
-            userId,
-            postId
-        })
-        await newComment.save();
-        res.status(200).json(newComment);
-
-    }catch(error){
-        next(error);
+export const createComment = async (req, res, next) => {
+  try {
+    const { userId, postId, content } = req.body;
+    if (userId !== req.user.id) {
+      return next(errorhandler(403, "you are not allowed to create a comment"));
     }
-}
+
+    const newComment = new Comment({
+      content,
+      userId,
+      postId,
+    });
+    await newComment.save();
+    res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,15 +1,17 @@
 import { Button, Textarea } from "flowbite-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { Alert } from "flowbite-react";
+import Comment from "./Comment";
 
 export default function Commentsection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
-
+  const [comments,setComments]=useState([])
+  console.log(comments);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -31,11 +33,27 @@ export default function Commentsection({ postId }) {
       if (res.ok) {
         setComment("");
         setCommentError(null);
+        setComments([data,...comments])
       }
     } catch (error) {
       setCommentError(error.message);
     }
   };
+
+  useEffect(()=>{
+      const getComments=async()=>{
+        try{
+          const res=await fetch(`/api/comment/getPostComments/${postId}`);
+        if(res.ok){
+          const data=await res.json();
+          setComments(data);
+        }
+        }catch(error){
+          console.log(error.message);
+        }    
+      }
+      getComments()
+  },[postId])
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -88,6 +106,27 @@ export default function Commentsection({ postId }) {
             </Alert>
           )}
         </form>
+      )}
+      {comments.length===0 ?(
+        <p className="text-gray-500 text-sm mt-5">No comments yet</p>
+      ):(
+        <>
+        <div className="text-sm my-5 flex items-center gap-1">
+          <p>Comments</p>
+          <div className="border border-gray-400 py-1 px-2 rounded-sm">
+           <p> {comments.length}</p>
+          </div>
+          </div>
+    
+          {
+            comments.map(comment=>(
+            <Comment 
+            key={comment._id}
+            comment={comment}
+            />
+          ))
+          }
+           </>
       )}
     </div>
   );
